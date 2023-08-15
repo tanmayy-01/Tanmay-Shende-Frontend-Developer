ef2ea3796c84d1e8c7e7b8f9961e2bdb43dda857
Tanmay Shende <80145478+tanmayy-01@users.noreply.github.com>
2023-08-15 12:43:57 +0530

    landing page consist banner

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../../components/Banner/banner'
+import Showcase from '../../components/Showcase/showcase'
+const Landing = () => {
+    return (
+        <>
+            <Banner />
+            <Showcase />
+        </>
+
+    )
+}
+
+export default Landing
